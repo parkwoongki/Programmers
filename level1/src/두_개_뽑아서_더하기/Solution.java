6bd95d35e3c7984e02a26c843d68dd339516f6bb
parkwoongki <sbe03005@naver.com>
2020-11-03 18:54:53 +0900

    두 개 뽑아서 더하기

---
+++ level1/src/두_개_뽑아서_더하기/Solution.java
@@ -0,0 +1,37 @@
+package 두_개_뽑아서_더하기;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public static int[] solution(int[] numbers) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                int number = numbers[i] + numbers[j];
+                if (!list.contains(number)) {
+                    list.add(number);
+                }
+            }
+        }
+
+        Collections.sort(list);
+
+        answer = list.stream().mapToInt(i -> i).toArray();
+
+//        for (Integer integer : list) {
+//            System.out.print(integer + " ");
+//        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 1, 3, 4, 1};
+
+        System.out.println("solution() = " + solution(numbers));
+    }
+}
