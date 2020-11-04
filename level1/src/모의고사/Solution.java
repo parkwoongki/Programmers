8ebbfd2d3057729c94562630879b4189158be0c1
parkwoongki <sbe03005@naver.com>
2020-11-04 20:15:09 +0900

    모의고사

---
+++ level1/src/모의고사/Solution.java
@@ -0,0 +1,47 @@
+package 모의고사;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] s3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] s = new int[3];
+
+        int l1 = s1.length;
+        int l2 = s2.length;
+        int l3 = s3.length;
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == s1[i % l1])
+                s[0]++;
+            if (answers[i] == s2[i % l2])
+                s[1]++;
+            if (answers[i] == s3[i % l3])
+                s[2]++;
+        }
+
+        int max = s[0];
+        for (int i = 1; i < 3; i++) {
+            max = Math.max(max, s[i]);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (s[i] == max) {
+                list.add(i + 1);
+            }
+        }
+        answer = list.stream().mapToInt(i -> i).toArray();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] answers = {1, 2, 3, 4, 5};
+
+        System.out.println(solution(answers));
+    }
+}
