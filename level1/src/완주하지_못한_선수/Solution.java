cbac40526b1455c5281d44d800cf669cd8bac36a
parkwoongki <sbe03005@naver.com>
2020-11-04 20:15:37 +0900

    완주하지 못한 선수

---
+++ level1/src/완주하지_못한_선수/Solution.java
@@ -0,0 +1,32 @@
+package 완주하지_못한_선수;
+
+import java.util.*;
+
+public class Solution {
+    public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+
+        if (answer.equals(""))
+            answer = participant[participant.length - 1];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"mislav", "stanko", "mislav"};
+
+        System.out.println(solution(participant, completion));
+    }
+}
---
+++ level1/src/완주하지_못한_선수/Solution2.java
@@ -0,0 +1,22 @@
+package 완주하지_못한_선수;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2 {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> hash = new HashMap<>();
+        for (String s : participant)
+            hash.put(s, hash.getOrDefault(s, 0) + 1);
+        for (String s : completion)
+            hash.put(s, hash.get(s) - 1);
+
+        for (String key : hash.keySet()) {
+            if (hash.get(key) != 0)
+                return key;
+        }
+
+        return null;
+    }
+}
